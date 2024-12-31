8645848bcad247373427b3a34eed67bb850b7dd7
AZunderstars <fidchetspinner@gmail.com>
2025-01-01 00:25:50 +0330

    created custom User model and its migration and manager and added to django settings

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,27 @@
-from django.db import models # noqa
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('Phone number must be set')
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(phone_number, password, **extra_fields)
+
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=20, unique=True)
+    username = None
+    USERNAME_FIELD = 'phone_number'
+    objects = UserManager()
