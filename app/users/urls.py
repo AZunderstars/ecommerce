d93814dba3c52ca7bcfea0298e13f83ed84f214b
AZunderstars <fidchetspinner@gmail.com>
2025-01-01 14:30:29 +0330

    added register to urls

--- app/users/urls.py
+++ app/users/urls.py
@@ -4,9 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from app.users import views
+
 app_name = 'users'
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
