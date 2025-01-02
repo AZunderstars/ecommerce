6f6791fbe87c4561d130bf00c6852f12536d5ef2
AZunderstars <fidchetspinner@gmail.com>
2025-01-02 11:19:53 +0330

    debug password in UserSerializer

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -7,3 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'phone_number', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
