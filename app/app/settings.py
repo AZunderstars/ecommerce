1a111350f6f1ed6376de19309169c0049d3bc21d
AZunderstars <fidchetspinner@gmail.com>
2025-01-03 21:14:49 +0330

    added deployment docker compose and changed dev docker compose and added sample .env file

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('POSTGRES_HOST'),
-        'NAME': os.environ.get('POSTGRES_DB'),
+        'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
     }
