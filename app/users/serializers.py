06b38b8c6131773925959d5f641b4c3596b9120f
AZunderstars <fidchetspinner@gmail.com>
2025-01-31 16:28:29 +0330

    added validation for UserSerializer fields

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import password_validation
+from django.core.validators import RegexValidator
 from rest_framework import serializers
 
 from core.models import User
@@ -11,3 +13,12 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+    def validate_password(self, value):
+        password_validation.validate_password(password=value)
+        return value
+
+    def validate_phone_number(self, value):
+        validator = RegexValidator(regex=r'^09[0-9]{9}$')
+        validator(value)
+        return value
